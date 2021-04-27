d2927d34023700fa3f54739bb502a904efd8eac3
Hums Shaik <humsshaik@gmail.com>
2021-04-27 18:06:10 -0500

    initial code ideas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart
+//= ================================
+
+// Calculate chart width and height
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+
