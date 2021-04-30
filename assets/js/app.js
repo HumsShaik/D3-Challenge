ac88b8a4139798dcd29931e51b49acd51a5ef947
Hums Shaik <humsshaik@gmail.com>
2021-04-30 11:59:31 -0500

    added code

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,42 @@ var height = svgHeight - margin.top - margin.bottom;
 //   and shift the latter by left and top margins.
 // ***************************************************
 
+const svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight );
+
+// append an SVG group that will hold our chart
+
+const chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+   // Initial Params
+var chosenXAxis = "poverty";
+var chosenYAxis = "healthcare";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(data, chosenXAxis) {
+  // create scales
+  const xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenXAxis]) * 0.9,
+      d3.max(data, d => d[chosenXAxis]) * 1.1
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating y-scale var upon click on axis label
+function yScale(data, chosenYAxis) {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenYAxis])-2,d3.max(data, d => d[chosenYAxis])+2])
+    .range([height, 0]);
+
+  return yLinearScale;
+
+}
+
